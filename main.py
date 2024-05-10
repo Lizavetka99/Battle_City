492f103b350c72bc2ddb578250eb4dfb6dfdf318
Elizaveta Pavlovskaya <lpavlovskayalp@mail.ru>
2024-05-10 20:07:22 +0500

    add test tank and background

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ pygame.display.set_caption("Battle City")
 icon = pygame.image.load("assets/game_icon.jpg")
 pygame.display.set_icon(icon)
 
+bg = pygame.image.load("assets/background_test.jpg")
+player = pygame.image.load("assets/tank.png")
+
+
 running = True
 while running:
     pygame.display.update()
 
+    screen.blit(bg, (0,0))
+    screen.blit(player, (100, 100))
     for event in pygame.event.get():
         if  event.type == pygame.QUIT:
             running = False
