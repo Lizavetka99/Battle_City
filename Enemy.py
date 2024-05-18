7b1da0b704c4a95e865ab28238466398a0974b45
Elizaveta Pavlovskaya <lpavlovskayalp@mail.ru>
2024-05-18 14:53:15 +0500

    add enemy textures

--- Enemy.py
+++ Enemy.py
@@ -12,19 +12,65 @@ DIRECTION = ["Up", "Down", "Left", "Right"]
 
 
 enemy_anim = {
-    "Up": [pygame.image.load("assets/bot/bot_1_w.png"),
-                 pygame.image.load("assets/bot/bot_2_w.png")],
-    "Down": [pygame.image.load("assets/bot/bot_1_s.png"),
-                 pygame.image.load("assets/bot/bot_2_s.png")],
-    "Right": [pygame.image.load("assets/bot/bot_1_d.png"),
-                 pygame.image.load("assets/bot/bot_2_d.png")],
-    "Left": [pygame.image.load("assets/bot/bot_1_a.png"),
-                 pygame.image.load("assets/bot/bot_2_a.png")]
+    "Up": [pygame.image.load("assets/enemy/bot/bot_1_w.png"),
+                 pygame.image.load("assets/enemy/bot/bot_2_w.png")],
+    "Down": [pygame.image.load("assets/enemy/bot/bot_1_s.png"),
+                 pygame.image.load("assets/enemy/bot/bot_2_s.png")],
+    "Right": [pygame.image.load("assets/enemy/bot/bot_1_d.png"),
+                 pygame.image.load("assets/enemy/bot/bot_2_d.png")],
+    "Left": [pygame.image.load("assets/enemy/bot/bot_1_a.png"),
+                 pygame.image.load("assets/enemy/bot/bot_2_a.png")]
 }
+
+enemy_anim_speed = {
+    "Up": [pygame.image.load("assets/enemy/fast/fast_enemy_1_w.png"),
+                 pygame.image.load("assets/enemy/fast/fast_enemy_2_w.png")],
+    "Down": [pygame.image.load("assets/enemy/fast/fast_enemy_1_s.png"),
+                 pygame.image.load("assets/enemy/fast/fast_enemy_2_s.png")],
+    "Right": [pygame.image.load("assets/enemy/fast/fast_enemy_1_d.png"),
+                 pygame.image.load("assets/enemy/fast/fast_enemy_2_d.png")],
+    "Left": [pygame.image.load("assets/enemy/fast/fast_enemy_1_a.png"),
+                 pygame.image.load("assets/enemy/fast/fast_enemy_2_a.png")]
+}
+
+enemy_anim_armor = {
+    "Up": [pygame.image.load("assets/enemy/armor/armored_enemy_1_w.png"),
+                 pygame.image.load("assets/enemy/armor/armored_enemy_2_w.png")],
+    "Down": [pygame.image.load("assets/enemy/armor/armored_enemy_1_s.png"),
+                 pygame.image.load("assets/enemy/armor/armored_enemy_2_s.png")],
+    "Right": [pygame.image.load("assets/enemy/armor/armored_enemy_1_d.png"),
+                 pygame.image.load("assets/enemy/armor/armored_enemy_2_d.png")],
+    "Left": [pygame.image.load("assets/enemy/armor/armored_enemy_1_a.png"),
+                 pygame.image.load("assets/enemy/armor/armored_enemy_2_a.png")]
+}
+enemy_anim_attack = {
+    "Up": [pygame.image.load("assets/enemy/bot/bot_1_w.png"),
+                 pygame.image.load("assets/enemy/bot/bot_2_w.png")],
+    "Down": [pygame.image.load("assets/enemy/bot/bot_1_s.png"),
+                 pygame.image.load("assets/enemy/bot/bot_2_s.png")],
+    "Right": [pygame.image.load("assets/enemy/bot/bot_1_d.png"),
+                 pygame.image.load("assets/enemy/bot/bot_2_d.png")],
+    "Left": [pygame.image.load("assets/enemy/bot/bot_1_a.png"),
+                 pygame.image.load("assets/enemy/bot/bot_2_a.png")]
+}
+
+
 for image in enemy_anim.keys():
     for i in range(len(enemy_anim[image])):
         enemy_anim[image][i] = \
             pygame.transform.scale(enemy_anim[image][i], (44, 44))
+for image in enemy_anim_speed.keys():
+    for i in range(len(enemy_anim_speed[image])):
+        enemy_anim_speed[image][i] = \
+            pygame.transform.scale(enemy_anim_speed[image][i], (44, 44))
+for image in enemy_anim_attack.keys():
+    for i in range(len(enemy_anim_attack[image])):
+        enemy_anim_attack[image][i] = \
+            pygame.transform.scale(enemy_anim_attack[image][i], (44, 44))
+for image in enemy_anim_armor.keys():
+    for i in range(len(enemy_anim_armor[image])):
+        enemy_anim_armor[image][i] = \
+            pygame.transform.scale(enemy_anim_armor[image][i], (44, 44))
 class Enemy:
     def __init__(self, pos_x, pos_y, speed, map, type):
         self.type = type
@@ -33,14 +79,28 @@ class Enemy:
         self.direction = "Down"
         self.pos_y = pos_y
         self.speed = speed
-        self.enemy_texture = enemy_anim["Down"][0]
+
         self.enemy_anim_count = 0
         self.size = 44
         self.map = map
         self.attack_delay = 0
         self.collider = pygame.rect.Rect(self.pos_x, self.pos_y, 40, 40)
-        if self.type == "armor": self.life = 2
-        else: self.life = 1
+        if self.type == "armor":
+            print(1)
+            self.life = 2
+            self.enemy_texture = enemy_anim_armor["Down"][0]
+        elif self.type == "speed":
+            print(2)
+            self.life = 1
+            self.enemy_texture = enemy_anim_speed["Down"][0]
+        elif self.type == "attack":
+            print(3)
+            self.life = 1
+            self.enemy_texture = enemy_anim_attack["Down"][0]
+        else:
+            print(4)
+            self.life = 1
+            self.enemy_texture = enemy_anim["Down"][0]
 
     def move(self):
         speed = 1
@@ -63,8 +123,16 @@ class Enemy:
             self.bullet.Move()
         if self.bullet.isCollision:
             self.bullet.Freeze_bullet()
-        self.enemy_texture = enemy_anim[self.direction][
-            self.enemy_anim_count]
+
+        if self.type == "usual":
+            self.enemy_texture = enemy_anim[self.direction][self.enemy_anim_count]
+        if self.type == "speed":
+            self.enemy_texture = enemy_anim_speed[self.direction][self.enemy_anim_count]
+        if self.type == "attack":
+            self.enemy_texture = enemy_anim_attack[self.direction][self.enemy_anim_count]
+        if self.type == "armor":
+            self.enemy_texture = enemy_anim_armor[self.direction][self.enemy_anim_count]
+
         if self.can_move():
             self.pos_x += MOVEMENT[self.direction][0] * speed
             self.pos_y += MOVEMENT[self.direction][1] * speed
